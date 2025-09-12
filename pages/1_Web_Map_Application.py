d984cd60fcbff663707442267e7cfc1b1b87d1fb
Atul Bhardwaj <atulmncfc@gmail.com>
2025-09-12 18:52:26 +0530

    Add _base_mapp

---
+++ pages/1_Web_Map_Application.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns([7, 3])
+
+options = list(leafmap.basemaps.keys())
+
+with col2:
+    #dropdown = st.selectbox("Basemap",["HYBRID", "ROADMAP","TERRAIN","SATELLITE"])
+    dropdown = st.selectbox("Basemap", options)
+    #url = st.text_input("Enter url", )
+    
+    default_url = leafmap.basemaps[dropdown].tiles
+    
+    url = st.text_input("Enter url", default_url)
+
+m =leafmap.Map()
+m.add_basemap(dropdown)
+
+if url: 
+    m.add_tile_layer(url, name="Tile Layer", attribution=' ')
+
+with col1:
+    m.to_streamlit()
+
